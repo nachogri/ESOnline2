68c52783489228c95f6adc80e1896ac6ad632585
NachoGri <nachogri@gmail.com>
2015-01-31 16:55:33 -0500

    Pagination completed

--- ESOnline2.WebUI/Scripts/Controllers/Cliente.js
+++ ESOnline2.WebUI/Scripts/Controllers/Cliente.js
@@ -1,7 +1,7 @@
 ﻿"use strict";
 
 angular.module('mdlControllers')
-    .controller('ctlCliente', function ($scope, svcESONlineUI, svcUtils, svcNotifications,svcBrowser) {
+    .controller('ctlCliente', function ($scope, svcESONlineUI, svcUtils, svcNotifications, svcBrowser) {
 
         $scope.cliente = {};        
         $scope.cliente.Telefonos = [];
@@ -10,10 +10,7 @@ angular.module('mdlControllers')
         $scope.cliente.Webs = [];
         $scope.clientes = [];
         $scope.nombreBusqueda = "";
-        $scope.ErrorList = "";
-        $scope.sizePagina = 10;
-        $scope.paginaActual = 1;
-        $scope.paginas = [];
+        $scope.ErrorList = "";        
 
         $scope.load = function () {
             switch (svcUtils.getAction()) {
@@ -30,9 +27,7 @@ angular.module('mdlControllers')
         $scope.getAllClientes = function () {
             svcESONlineUI.clientes.getAll()
                 .success(function (data) {
-                    $scope.clientes = data;
-                   
-                    CalcularPaginas();
+                    $scope.clientes = data;                                       
                 })
                 .error(function (err) {
                     svcNotifications.alert("Ha ocurrido un error:" + err);
@@ -121,38 +116,7 @@ angular.module('mdlControllers')
                 $scope.errors.pageError = "An unexpected error has occurred, please try again later.";
                 svcNotifications.alert('Error',$scope.errors.pageError);
             }            
-        };       
-      
-        $scope.CambiarSizePagina = function (data) {
-            $scope.sizePagina = data;
-            CalcularPaginas();
-        }
-
-        $scope.CambiarPagina = function (data) {
-            if (data > $scope.paginaActual && $scope.paginaActual < ($scope.paginas.length-1))
-                $scope.paginaActual = data;
-
-            if (data < $scope.paginaActual && $scope.paginaActual > 0)
-                $scope.paginaActual = data;
-        }
-
-        function CalcularPaginas() {            
-            $scope.paginas = [];
-            $scope.paginas[0] = 1;
-            for (var i = 2; i < ($scope.clientes.length / $scope.sizePagina) + 1 ; i++) {
-                $scope.paginas[i - 1] = i;
-            }
-            $scope.paginaActual = 0;
-        }
-
+        };                                    
         $scope.load();
-
     })
 
-angular.module('mdlControllers')
-    .filter('offset', function () {
-        return function (input, start) {
-            start = parseInt(start, 10);
-            return input.slice(start);
-        };
-    });
--- ESOnline2.WebUI/Scripts/Modules/ESONlineApp.js
+++ ESOnline2.WebUI/Scripts/Modules/ESONlineApp.js
@@ -1,5 +1,6 @@
 ﻿'use strict';
 
 angular.module('mdlControllers', []);
-var app = angular.module('mdlESOnlineApp', ['ngRoute', 'mdlControllers']);
+angular.module('mdlESOPagination', []);
+var app = angular.module('mdlESOnlineApp', ['ngRoute', 'mdlControllers', 'mdlESOPagination']);
 
