7614141b5625d19dc6a6dc1a891be7e6687f1a03
NachoGri <nachogri@gmail.com>
2015-02-16 11:44:35 -0500

    Tabs Navigator Done

---
+++ ESOnline2.WebUI/Scripts/Controllers/TabNavigator.js
@@ -0,0 +1,20 @@
+﻿"use strict";
+
+angular.module('mdlControllers')
+    .controller('ctlTab', function ($scope) {
+         
+        $scope.Tabs = [
+            { nombre: "Datos", selected: true},
+            { nombre: "Productos", selected: false}        
+        ]
+
+        $scope.changeTab = function (tab) {           
+            for (var i = 0; i < $scope.Tabs.length; i++) {
+                if ($scope.Tabs[i].nombre==tab.nombre)
+                    $scope.Tabs[i].selected = true;
+                else
+                    $scope.Tabs[i].selected = false;                
+            }            
+        }        
+    })
+
