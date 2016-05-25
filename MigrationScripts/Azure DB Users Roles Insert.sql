a1eac55c73835dda552eb3d26926dfcdbaea887a
NachoGri <nachogri@gmail.com>
2016-05-25 10:20:08 -0300

    Data Migration scripts

---
+++ MigrationScripts/Azure DB Users Roles Insert.sql
@@ -0,0 +1,4 @@
+﻿INSERT INTO [dbo].[webpages_UsersInRoles] ([UserId], [RoleId]) VALUES (1, 1)
+INSERT INTO [dbo].[webpages_UsersInRoles] ([UserId], [RoleId]) VALUES (2, 1)
+INSERT INTO [dbo].[webpages_UsersInRoles] ([UserId], [RoleId]) VALUES (1003, 1)
+INSERT INTO [dbo].[webpages_UsersInRoles] ([UserId], [RoleId]) VALUES (3002, 2)
