d398b8d31a08905d3d587026573787556d2e7ba6
NachoGri <nachogri@gmail.com>
2016-06-19 11:33:43 -0300

    Removing sensitive files

--- MigrationScripts/Azure DB Users Profiles Insert.sql
+++
@@ -1,5 +0,0 @@
-﻿SET IDENTITY_INSERT [dbo].[UserProfile] ON
-INSERT INTO [dbo].[UserProfile] ([UserId], [UserName]) VALUES (1, N'Nacho')
-INSERT INTO [dbo].[UserProfile] ([UserId], [UserName]) VALUES (2, N'Pablo')
-INSERT INTO [dbo].[UserProfile] ([UserId], [UserName]) VALUES (1003, N'Ezequiel')
-SET IDENTITY_INSERT [dbo].[UserProfile] OFF
